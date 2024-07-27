2afdae7d48e080270da27f5c2e06586dbcec47f4
kumar <prem889940@gmail.com>
2024-07-27 16:08:27 +0530

    project created

---
+++ src/main/java/com/nit/ctrl/RestApi.java
@@ -0,0 +1,31 @@
+package com.nit.ctrl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestApi {
+	
+	Logger log=LoggerFactory.getLogger(RestApi.class);
+	@GetMapping("/")
+	public String welcome() {
+		log.info("******Welcome Method started***********");
+		log.info("******Welcome Method finished***********");
+	
+		return "<h1 style=color:red;text-align:center>Welcome to Rest Controller Application</h1>";
+	}
+	
+	@GetMapping("/wish/{name}")
+	public String wish(@PathVariable String name) {
+		log.info("******Wish Method started***********");
+		log.info("******Wish Method ended***********");
+
+	
+		return "<h1 style=color:green;text-align:center>Hi Mr/mrs "+name+" Welcome </h1>";
+	}
+	
+
+}
